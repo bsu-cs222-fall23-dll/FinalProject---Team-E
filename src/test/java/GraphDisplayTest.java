6988b20ffde44e005864cda44e37a2fa7d35a18b
sunghoo05 <121914751+sunghoo05@users.noreply.github.com>
2023-10-26 23:02:26 -0400

    Update GraphDisplayTest.java

--- src/test/java/GraphDisplayTest.java
+++ src/test/java/GraphDisplayTest.java
@@ -2,7 +2,9 @@
 Authors: Alexander Bousman, Sung Hoo Hong, David Chen
 */
 
-public class GraphDisplayTest 
+public class GraphDisplayTest
 {
-  
+    public static void main(String[] args) {
+    new GraphingCalculator();
+}
 }
